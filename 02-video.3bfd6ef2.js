e68489a91f5f2bfbd6b8a677dfdef4808e2545fd
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-02 17:16:58 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-08@98a8df438c736f6d36d9b676b5c74e448a5180bf 🚀

---
+++ 02-video.3bfd6ef2.js
@@ -0,0 +1,2 @@
+localStorage.setItem("Test_key",JSON.stringify({name:"Radar",price:1300,quantity:4})),console.log(JSON.parse(localStorage.getItem("Test_key")));
+//# sourceMappingURL=02-video.3bfd6ef2.js.map
