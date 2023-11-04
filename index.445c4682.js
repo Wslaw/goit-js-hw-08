7ef1d7137922e4a7081d83ea75e0b25addf71970
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-04 16:09:28 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-08@94731b96614ffa6db9f56d6c9e62059a19399921 🚀

---
+++ index.445c4682.js
@@ -0,0 +1,2 @@
+var numStars=1e3;function createStars(){for(var t=0;t<numStars;t++){var a=document.createElement("div");a.classList.add("star"),a.style.left="".concat(Math.random()*window.innerWidth,"px"),a.style.top="".concat(Math.random()*window.innerHeight,"px"),document.body.appendChild(a)}}createStars();
+//# sourceMappingURL=index.445c4682.js.map
